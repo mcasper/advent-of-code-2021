855721ff3f5b4e6eeca7f354e95e7b4493d09ee6
Matt Casper <matthewvcasper@gmail.com>
2021-12-09 20:20:58 -0800

    Day 7, Part 2

---
+++ day7/part2.cpp
@@ -0,0 +1,63 @@
+#include <math.h>
+#include <string>
+#include <vector>
+
+#include "../utils/io.h"
+#include "../utils/string_scanner.h"
+
+int _solve(std::string path) {
+    std::string input = stringListInput(path)[0];
+    std::vector<int> positions;
+    StringScanner scanner(input);
+
+    while (!scanner.eof()) {
+        positions.push_back(scanner.parseInt());
+        scanner.parseCharacter(',');
+    }
+
+    int maxPosition = 0;
+    int minPosition = -1;
+    for (int position : positions) {
+        maxPosition = std::max(maxPosition, position);
+
+        if (minPosition == -1) {
+            minPosition = position;
+        } else {
+            minPosition = std::min(minPosition, position);
+        }
+    }
+
+    int minFuelCost = -1;
+    for (int i = minPosition; i <= maxPosition; i++) {
+        int fuelCost = 0;
+
+        for (int position : positions) {
+            int travelDistance = std::abs(position - i);
+            fuelCost += (pow(travelDistance, 2) + travelDistance) / 2;
+        }
+
+        if (minFuelCost == -1) {
+            minFuelCost = fuelCost;
+        } else {
+            minFuelCost = std::min(minFuelCost, fuelCost);
+        }
+    }
+
+    return minFuelCost;
+}
+
+int solve() {
+    return _solve("../day7/input.txt");
+}
+
+#ifdef MY_TEST
+
+#include <gtest/gtest.h>
+
+TEST(Day7Part1Test, Works) {
+    int expected = 168;
+    int actual = _solve("../day7/sample.txt");
+    EXPECT_EQ(expected, actual);
+}
+
+#endif
